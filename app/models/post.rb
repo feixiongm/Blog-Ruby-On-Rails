fbe1f25f9ac17154cde436b1bdf7f5cdcf94a4bb
feixiongm <feixiongm@gwu.edu>
2020-12-01 00:33:59 -0500

    add asset pipline

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,34 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :summary, presence: true, length: {maximum: 200 }
   validates :body, presence: true, length:{ minimum: 1 }
+
+  belongs_to :category
+
+  after_create :update_total_posts_count
+
+  scope :active, -> { where( active: true) }
+  scope :order_by_latest_first, -> { order( created_at: :desc )}
+  scope :limit_2, -> { limit( 2 ) }
+
+  private
+
+  def update_total_posts_count
+    category.increment(:total_count, 1).save
+
+  end
   # validates :active, in: [ true, false ]
   # validates :terms_of_service, acceptance: true
   # validates :rating, numericality: { only_integer: true }
-
-  def details
-    "This post was created on #{created_at.strftime("%d/%m%Y")}"
-  end
-
-  def self.total
-    #return a total number of posts
-    count
-  end
+  # belongs_to :user
+  # has_many :comments
+  # def details
+  #   "This post was created on #{created_at.strftime("%d/%m%Y")}"
+  # end
+  #
+  # def self.total
+  #   #return a total number of posts
+  #   all.size
+  #   #count
+  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_071321) do
+ActiveRecord::Schema.define(version: 2020_12_01_034632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_071321) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "total_count"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,6 +30,9 @@ ActiveRecord::Schema.define(version: 2020_11_30_071321) do
     t.boolean "active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
+  add_foreign_key "posts", "categories"
 end
